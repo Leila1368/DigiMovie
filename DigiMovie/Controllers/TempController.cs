89ad727415ee692bc860d4e033b2b3fc4a886373
Asp.Net <Leila_n1368@yahoo.com>
2017-08-08 12:21:59 +0430

    primary

---
+++ DigiMovie/Controllers/TempController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiMovie.Controllers
+{
+    public class TempController : Controller
+    {
+        // GET: Temp
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("Gallery/{year:int:range(1300,1400)}/{month:int:range(1,12)}")]
+        public ActionResult ShowAlbum(int year,int month)
+        {
+            // return View();
+            return Content(year + "<hr>" + month);
+        }
+    }
+}
